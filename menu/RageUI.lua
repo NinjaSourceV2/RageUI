5f8d307012f063a1c7b69ed216498568218f0372
Ninja <samir932205@gmail.com>
2021-04-23 20:54:27 +0200

    add delay submenu item.

--- menu/RageUI.lua
+++ menu/RageUI.lua
@@ -544,6 +544,7 @@ function RageUI.Render()
             end
             if RageUI.NextMenu ~= nil then
                 if RageUI.NextMenu() then
+                    Wait(1) --> New Wait added to not execute the next button while clicked on a sub menu.
                     RageUI.Visible(CurrentMenu, false)
                     RageUI.Visible(RageUI.NextMenu, true)
                     CurrentMenu.Controls.Select.Active = false
